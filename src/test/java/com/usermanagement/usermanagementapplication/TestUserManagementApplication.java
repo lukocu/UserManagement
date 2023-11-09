3eefd7b288abae895c1b3ddc3005a59d8093ad7b
lukoc <lukasz123237@gmail.com>
2023-11-09 11:01:45 +0100

    init project

---
+++ src/test/java/com/usermanagement/usermanagementapplication/TestUserManagementApplication.java
@@ -0,0 +1,24 @@
+package com.usermanagement.usermanagementapplication;
+
+import com.usermanagement.UserManagementApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestUserManagementApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(UserManagementApplication::main).with(TestUserManagementApplication.class).run(args);
+	}
+
+}
